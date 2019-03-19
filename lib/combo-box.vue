e7862efa0bd6a6c939e7b9f75e54a3e53212c3dc
scrollbar <scrollbar@163.com>
2019-03-19 11:47:14 +0800

    + clear() @combo

--- lib/combo-box.vue
+++ lib/combo-box.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="input-box dropdown-group" :readonly="readonly" sparrow-popup :popup-action="popupAction" @optionclick.stop="onOptionClick">
+  <div class="input-box dropdown-group" :readonly="readonly" :buttons="buttons"
+    sparrow-popup :popup-action="popupAction" @optionclick.stop="onOptionClick">
     <input type="text" :placeholder="placeholder" v-model="inputValue" :readonly="readonly || isDropdownList">
+    <a toggle-type="clear" popup-action="close" v-if="buttons" @click="clear"></a>
     <a toggle-type="expand" popup-action="toggle"></a>
     <div class="dropdown list-group" v-if="!readonly && options && options.length">
       <slot>
@@ -27,6 +29,9 @@
       }
     },
     computed: {
+      buttons () {
+        return (this.readonly || !this.inputValue) ? undefined : '2'
+      },
       isDropdownList () {
         return !this.dropdownStyle || this.dropdownStyle.toLowerCase() === 'dropdownlist'
       },
@@ -86,6 +91,9 @@
         } else {
           this.$emit('change', value)
         }
+      },
+      clear () {
+        this.$emit('change', this.multiple ? [] : '')
       },
       expand () {
         show(this.$el)
