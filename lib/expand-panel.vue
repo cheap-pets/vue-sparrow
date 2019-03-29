047cce1a1e8d9622962481908c051b21528721ba
scrollbar <scrollbar@163.com>
2019-03-29 15:26:52 +0800

    增加 form-field 和 expand-panel

---
+++ lib/expand-panel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="expand-panel" :expanded="expandState">
+      <slot></slot>
+    </div>
+    <slot name="trigger">
+      <div class="bar" justify-content="center">
+        <a class="text-link" toggle-type="expand" :expanded="expandState" @click="onToggle">
+          {{ expanded ? collapseTitle : expandTitle }}
+        </a>
+      </div>
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SuExpandPanel',
+    props: [ 'expanded', 'expandTitle', 'collapseTitle' ],
+    data () {
+      return {
+        expandState: this.expanded
+      }
+    },
+    methods: {
+      onToggle () {
+        this.expandState = this.expandState ? undefined : 'expanded'
+      }
+    },
+    watch: {
+      expanded (val) {
+        this.expandState = val
+      }
+    }
+  }
+</script>
---
+++ lib/form-field.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="form-field">
+    <slot name="label">
+      <label :style="{ width: labelWidth }">{{ label }}</label>
+    </slot>
+    <slot>
+      <input type="text" v-model="value" :readonly="readonly" no-border>
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SuFormField',
+    props: [ 'label', 'value', 'readonly', 'labelWidth' ],
+    model: {
+      prop: 'value',
+      event: 'input'
+    }
+  }
+</script>
